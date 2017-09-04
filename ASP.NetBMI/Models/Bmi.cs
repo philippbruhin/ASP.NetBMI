fae0cb804d2f1a48eb6542ea27f38dde5c88ce59
philippbruhin <info@philippbruhin.ch>
2017-09-04 12:56:34 +0200

    Added model and input form

--- ASP.NetBMI/Models/Bmi.cs
+++ ASP.NetBMI/Models/Bmi.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP.NetBMI.Models
 {
     public class Bmi
     {
+        [Display(Name = "Weight in kg")]
+        public double Weight { get; set; }
+
+        [Display(Name = "Height in cm")]
+        public double Height { get; set; }
     }
 }
