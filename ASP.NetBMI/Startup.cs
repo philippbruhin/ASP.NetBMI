20f63e4edb77489ad65d0204dee92135bb5e2b79
philippbruhin <info@philippbruhin.ch>
2017-09-05 09:17:22 +0200

    added service to DI Container

--- ASP.NetBMI/Startup.cs
+++ ASP.NetBMI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NetBMI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,7 @@ namespace ASP.NetBMI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IBmiService, BmiService>();
             services.AddMvc();
         }
 
